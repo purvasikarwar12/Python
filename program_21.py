fa6ead42257581db72950307b069c371f650a0e4
Purva12 <60695849+purvasikarwar12@users.noreply.github.com>
2020-11-05 20:20:42 +0530

    while loop(demo 1)

---
+++ program_21.py
@@ -0,0 +1,17 @@
+n=0
+while n!=1:
+     print("You are inside the while loop.")
+     print("Press 1 to exit from the loop:")
+     n=int(input())
+print("You are out of the while loop.")
+
+
+#OUTPUT:-
+
+ #You are inside the while loop.
+ #Press 1 to exit from the loop:
+ #6
+ #You are inside the while loop.
+ #Press 1 to exit from the loop:
+ #1
+ #You are out of the while loop.
