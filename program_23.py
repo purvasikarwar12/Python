ee86ef7d850a138a0df57913284232fd2a1669f5
Purva12 <60695849+purvasikarwar12@users.noreply.github.com>
2020-11-05 20:54:14 +0530

    while loop(square of number)

---
+++ program_23.py
@@ -0,0 +1,16 @@
+num=1
+while num <5:
+    print(num,"is less than 5")
+    num +=1
+else:
+    print("We reached to 5")
+
+
+#OUTPUT:-
+
+ #1 is less than 5
+ #2 is less than 5
+ #3 is less than 5
+ #4 is less than 5
+ #5 is less than 5
+ #We reached to 5
