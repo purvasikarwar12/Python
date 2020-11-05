035325f3138f8ea22ddc1b11c58e4da43c44e0d9
Purva12 <60695849+purvasikarwar12@users.noreply.github.com>
2020-11-05 19:48:16 +0530

    for loop-print even numbers

---
+++ program_14.py
@@ -0,0 +1,21 @@
+print("Even numbers between 1 to 25:")
+for x in range(1,26):
+    if(x%2==0):
+      print(x)
+
+
+#OUTPUT:-
+
+#Even numbers between 1 to 25:
+#2
+#4
+#6
+#8
+#10
+#12
+#14
+#16
+#18
+#20
+#22
+#24
