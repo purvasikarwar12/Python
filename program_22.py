3f0db4e1135eb291fcbfdfafc9ebd3caa916e361
Purva12 <60695849+purvasikarwar12@users.noreply.github.com>
2020-11-05 20:53:12 +0530

    while loop(print no.)

---
+++ program_22.py
@@ -0,0 +1,16 @@
+n=1
+while n !=0:
+    n=int(input("Enter a number:"))
+    print("Square of the number is ",n*n)
+print("End of Program")
+
+
+#OUTPUT:-
+
+ #Enter a number:5
+ #Square of the number is 25
+ #Enter a number:3
+ #Square of the number is 9
+ #Enter a number:0
+ #Square of the number is 0
+ #End of Program
