0fcb2944335f8ccbe84cc4f88cd1c4b37271cc7a
Purva12 <60695849+purvasikarwar12@users.noreply.github.com>
2020-11-08 21:00:42 +0530

    Swap two number without using third variable

---
+++ program_36.py
@@ -0,0 +1,11 @@
+a=int(input("Enter value of first variable: "))
+b=int(input("Enter value of second variable: "))
+a=a+b
+b=a-b
+a=a-b
+print("a is:",a," b is:",b)
+
+#OUTPUT:-
+#Enter value of first variable: 5 
+#Enter value of second variable: 7 
+#a is: 7 b is: 5
