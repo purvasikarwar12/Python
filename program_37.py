e21245b027692e7c6a4c3cb535365da6d28b147b
Purva12 <60695849+purvasikarwar12@users.noreply.github.com>
2020-11-08 21:01:36 +0530

    Swap two numbers with using third variable

---
+++ program_37.py
@@ -0,0 +1,25 @@
+def recur_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return(recur_fibo(n-1) + recur_fibo(n-2))
+nterms = 10
+if nterms <= 0:
+    print("Plese enter a positive integer")
+else:
+    print("Fibonacci sequence:")
+    for i in range(nterms):
+        print(recur_fibo(i))
+
+#OUTPUT:-
+#Fibonacci sequence: 
+#0 
+#1 
+#1 
+#2 
+#3 
+#5 
+#8 
+#13 
+#21 
+#34
